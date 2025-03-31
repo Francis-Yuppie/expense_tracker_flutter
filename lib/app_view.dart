595c16ab0865c7c326fa021429b2f87ab8c1e324
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2025-04-01 02:39:28 +0300

    initial commit

---
+++ lib/app_view.dart
@@ -0,0 +1,23 @@
+import 'package:expense_tracker/screens/home/views/home_screen.dart';
+import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Expense Tracker",
+      theme: ThemeData(
+          colorScheme: ColorScheme.light(
+              surface: Colors.grey.shade100,
+              onSurface: Colors.black,
+              primary: Color(0xFF00B2E7),
+              secondary: Color(0xFFE064F7),
+              tertiary: Color(0xFFFF8D6C))),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/app.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+ 
